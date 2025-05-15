1be7b95d05e632332f79af1cef3b3f0082f3450b
Stanisław Krowicki <krowicki00@gmail.com>
2025-05-15 19:59:47 +0200

    Move model loading to a separate file

---
+++ model.cpp
@@ -0,0 +1,224 @@
+#include "model.hpp"
+#include <iostream>
+#include "stb_image.h"
+#include <cstring>
+#include "assimp/Importer.hpp"
+#include "assimp/postprocess.h"
+
+unsigned int TextureFromMemory(const unsigned char* dataBuffer, const size_t dataSize, const std::string& nameHint) {
+    unsigned int textureID;
+    glGenTextures(1, &textureID);
+
+    int width, height, nrComponents;
+    unsigned char *data = stbi_load_from_memory(dataBuffer, static_cast<int>(dataSize), &width, &height, &nrComponents, 0);
+
+    if (data) {
+        const GLenum format = (nrComponents == 1) ? GL_RED : (nrComponents == 3) ? GL_RGB : GL_RGBA;
+
+        glBindTexture(GL_TEXTURE_2D, textureID);
+        glTexImage2D(GL_TEXTURE_2D, 0, static_cast<GLint>(format), width, height, 0, format, GL_UNSIGNED_BYTE, data);
+        glGenerateMipmap(GL_TEXTURE_2D);
+
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+
+        stbi_image_free(data);
+    } else {
+        std::cerr << "Embedded texture failed to load";
+        if (!nameHint.empty())
+            std::cerr << " (" << nameHint << ")";
+        std::cerr << std::endl;
+    }
+
+    return textureID;
+}
+
+unsigned int TextureFromFile(const char *path, const std::string &directory) {
+    const std::string filename = directory + '/' + path;
+
+    unsigned int textureID;
+    glGenTextures(1, &textureID);
+
+    int width, height, nrComponents;
+    unsigned char *data = stbi_load(filename.c_str(), &width, &height, &nrComponents, 0);
+    if (data) {
+        const GLenum format = (nrComponents == 1) ? GL_RED : (nrComponents == 3) ? GL_RGB : GL_RGBA;
+
+        glBindTexture(GL_TEXTURE_2D, textureID);
+        glTexImage2D(GL_TEXTURE_2D, 0, static_cast<GLint>(format), width, height, 0, format, GL_UNSIGNED_BYTE, data);
+        glGenerateMipmap(GL_TEXTURE_2D);
+
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+
+        stbi_image_free(data);
+    } else {
+        std::cout << "Texture failed to load at path: " << path << std::endl;
+        stbi_image_free(data);
+    }
+
+    return textureID;
+}
+
+Mesh::Mesh(std::vector<Vertex> vertices, std::vector<unsigned int> indices, std::vector<Texture> textures)
+    : vertices(std::move(vertices)), indices(std::move(indices)), textures(std::move(textures)) {
+    setupMesh();
+}
+
+void Mesh::setupMesh() {
+    glGenVertexArrays(1, &VAO);
+    glGenBuffers(1, &VBO);
+    glGenBuffers(1, &EBO);
+
+    glBindVertexArray(VAO);
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    glBufferData(GL_ARRAY_BUFFER, static_cast<long>(vertices.size() * sizeof(Vertex)), &vertices[0], GL_STATIC_DRAW);
+
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, static_cast<long>(indices.size() * sizeof(unsigned int)), &indices[0], GL_STATIC_DRAW);
+
+    glEnableVertexAttribArray(0);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, sizeof(Vertex), (void*)0);
+
+    glEnableVertexAttribArray(1);
+    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, sizeof(Vertex), (void*)offsetof(Vertex, Normal));
+
+    glEnableVertexAttribArray(2);
+    glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, sizeof(Vertex), (void*)offsetof(Vertex, TexCoords));
+
+    glBindVertexArray(0);
+}
+
+void Mesh::Draw(const Shader &shader) const {
+    unsigned int diffuseNr = 1;
+    unsigned int specularNr = 1;
+
+    for (unsigned int i = 0; i < textures.size(); ++i) {
+        glActiveTexture(GL_TEXTURE0 + i);
+        std::string number;
+        std::string name = textures[i].type;
+
+        if (name == "texture_diffuse")
+            number = std::to_string(diffuseNr++);
+        else if (name == "texture_specular")
+            number = std::to_string(specularNr++);
+
+        glUniform1i(shader.u("material." + name + number), static_cast<GLint>(i));
+        glBindTexture(GL_TEXTURE_2D, textures[i].id);
+    }
+
+    glActiveTexture(GL_TEXTURE0);
+    glBindVertexArray(VAO);
+    glDrawElements(GL_TRIANGLES, indices.size(), GL_UNSIGNED_INT, nullptr);
+    glBindVertexArray(0);
+}
+
+Model::Model(const std::string& path) {
+    loadModel(path);
+}
+
+void Model::Draw(Shader &shader) {
+    for (auto &mesh : meshes)
+        mesh.Draw(shader);
+}
+
+void Model::loadModel(const std::string& path) {
+    std::cout << "Loading model " << path << std::endl;
+
+    Assimp::Importer import;
+    const aiScene *scene = import.ReadFile(path, aiProcess_Triangulate | aiProcess_PreTransformVertices);
+
+    if (!scene || scene->mFlags & AI_SCENE_FLAGS_INCOMPLETE || !scene->mRootNode) {
+        std::cout << "ERROR::ASSIMP::" << import.GetErrorString() << std::endl;
+        return;
+    }
+    directory = path.substr(0, path.find_last_of('/'));
+    processNode(scene->mRootNode, scene);
+
+    std::cout << "Finished loading model" << path << std::endl;
+}
+
+void Model::processNode(const aiNode *node, const aiScene *scene) {
+    for (unsigned int i = 0; i < node->mNumMeshes; i++) {
+        aiMesh *mesh = scene->mMeshes[node->mMeshes[i]];
+        meshes.push_back(processMesh(mesh, scene));
+    }
+    for (unsigned int i = 0; i < node->mNumChildren; i++) {
+        processNode(node->mChildren[i], scene);
+    }
+}
+
+Mesh Model::processMesh(const aiMesh *mesh, const aiScene *scene) {
+    std::vector<Vertex> vertices;
+    std::vector<unsigned int> indices;
+    std::vector<Texture> textures;
+
+    for (unsigned int i = 0; i < mesh->mNumVertices; i++) {
+        Vertex vertex{};
+        vertex.Position = glm::vec3(mesh->mVertices[i].x, mesh->mVertices[i].y, mesh->mVertices[i].z);
+        vertex.Normal = glm::vec3(mesh->mNormals[i].x, mesh->mNormals[i].y, mesh->mNormals[i].z);
+
+        if (mesh->mTextureCoords[0])
+            vertex.TexCoords = glm::vec2(mesh->mTextureCoords[0][i].x, mesh->mTextureCoords[0][i].y);
+        else
+            vertex.TexCoords = glm::vec2(0.0f, 0.0f);
+
+        vertices.push_back(vertex);
+    }
+
+    for (unsigned int i = 0; i < mesh->mNumFaces; i++) {
+        for (unsigned int j = 0; j < mesh->mFaces[i].mNumIndices; j++)
+            indices.push_back(mesh->mFaces[i].mIndices[j]);
+    }
+
+    aiMaterial *material = scene->mMaterials[mesh->mMaterialIndex];
+    auto diffuseMaps = loadMaterialTextures(material, scene, aiTextureType_DIFFUSE, "texture_diffuse");
+    textures.insert(textures.end(), diffuseMaps.begin(), diffuseMaps.end());
+    auto specularMaps = loadMaterialTextures(material, scene, aiTextureType_SPECULAR, "texture_specular");
+    textures.insert(textures.end(), specularMaps.begin(), specularMaps.end());
+
+    return {vertices, indices, textures};
+}
+
+std::vector<Texture> Model::loadMaterialTextures(const aiMaterial *mat, const aiScene *scene, const aiTextureType type, const std::string& typeName) {
+    std::vector<Texture> textures;
+    for (unsigned int i = 0; i < mat->GetTextureCount(type); i++) {
+        aiString str;
+        mat->GetTexture(type, i, &str);
+
+        auto it = std::find_if(loadedTextures.begin(), loadedTextures.end(), [&](const Texture& t) {
+            return std::strcmp(t.path.c_str(), str.C_Str()) == 0;
+        });
+
+        if (it != loadedTextures.end()) {
+            textures.push_back(*it);
+            continue;
+        }
+
+        Texture texture;
+        if (str.C_Str()[0] == '*') {
+            const int textureIndex = std::stoi(str.C_Str() + 1);
+            aiTexture* embeddedTexture = scene->mTextures[textureIndex];
+
+            if (embeddedTexture->mHeight == 0) {
+                texture.id = TextureFromMemory(reinterpret_cast<unsigned char*>(embeddedTexture->pcData), embeddedTexture->mWidth, str.C_Str());
+            } else {
+                std::cout << "Tried to load unknown texture format (uncompressed): " << str.C_Str() << std::endl;
+                continue;
+            }
+        } else {
+            texture.id = TextureFromFile(str.C_Str(), directory);
+        }
+
+        texture.type = typeName;
+        texture.path = str.C_Str();
+        textures.push_back(texture);
+        loadedTextures.push_back(texture);
+    }
+
+    return textures;
+}
---
+++ model.hpp
@@ -0,0 +1,52 @@
+#pragma once
+
+#include <vector>
+#include <string>
+#include <glm/glm.hpp>
+#include <assimp/scene.h>
+#include "shader.hpp"
+
+struct Vertex {
+    glm::vec3 Position;
+    glm::vec3 Normal;
+    glm::vec2 TexCoords;
+};
+
+struct Texture {
+    unsigned int id;
+    std::string type;
+    std::string path;
+};
+
+class Mesh {
+public:
+    std::vector<Vertex> vertices;
+    std::vector<unsigned int> indices;
+    std::vector<Texture> textures;
+
+    Mesh(std::vector<Vertex> vertices, std::vector<unsigned int> indices, std::vector<Texture> textures);
+    void Draw(const Shader &shader) const;
+
+private:
+    unsigned int VAO{}, VBO{}, EBO{};
+    void setupMesh();
+};
+
+class Model {
+public:
+    explicit Model(const std::string& path);
+    void Draw(Shader &shader);
+
+private:
+    std::vector<Mesh> meshes;
+    std::string directory;
+    std::vector<Texture> loadedTextures;
+
+    void loadModel(const std::string& path);
+    void processNode(const aiNode *node, const aiScene *scene);
+    Mesh processMesh(const aiMesh *mesh, const aiScene *scene);
+    std::vector<Texture> loadMaterialTextures(const aiMaterial *mat, const aiScene *scene, aiTextureType type, const std::string& typeName);
+};
+
+unsigned int TextureFromMemory(const unsigned char* dataBuffer, size_t dataSize, const std::string& nameHint = "");
+unsigned int TextureFromFile(const char *path, const std::string &directory);
