775f900e3ebe50444a327d3713fa3d928881cdf3
Stanisław Krowicki <krowicki00@gmail.com>
2025-05-15 20:37:03 +0200

    Clean up main file

--- main.cpp
+++ main.cpp
@@ -16,20 +16,8 @@ void errorCallback(int error, const char* description) {
     fputs(description, stderr);
 }
 
-float vertices[] = {
-     0.5f,  0.5f, 0.0f,  // top right
-     0.5f, -0.5f, 0.0f,  // bottom right
-    -0.5f, -0.5f, 0.0f,  // bottom left
-    -0.5f,  0.5f, 0.0f   // top left
-};
-
-unsigned int indices[] = {  // note that we start from 0!
-    0, 1, 3,   // first triangle
-    1, 2, 3    // second triangle
-};
-
-Camera camera(glm::vec3(0.0f, 20.0f, 3.0f));
-Model *backpackModel;
+Camera camera(glm::vec3(0.0f, 5.0f, 0.0f));
+Model *trackModel;
 float lastX = 800.0f / 2.0f;
 float lastY = 600.0f / 2.0f;
 bool firstMouse = true;
@@ -53,47 +41,41 @@ void processInput(GLFWwindow *window)
         camera.ProcessKeyboard(RIGHT, deltaTime);
 }
 
-// glfw: whenever the window size changed (by OS or user resize) this callback function executes
-// ---------------------------------------------------------------------------------------------
 void framebuffer_size_callback(GLFWwindow* window, int width, int height)
 {
-    // make sure the viewport matches the new window dimensions; note that width and
-    // height will be significantly larger than specified on retina displays.
     glViewport(0, 0, width, height);
 }
 
-// glfw: whenever the mouse moves, this callback is called
-// -------------------------------------------------------
 void mouse_callback(GLFWwindow* window, double xposIn, double yposIn)
 {
-    float xpos = static_cast<float>(xposIn);
-    float ypos = static_cast<float>(yposIn);
+    auto xPos = static_cast<float>(xposIn);
+    auto yPos = static_cast<float>(yposIn);
 
     if (firstMouse)
     {
-        lastX = xpos;
-        lastY = ypos;
+        lastX = xPos;
+        lastY = yPos;
         firstMouse = false;
     }
 
-    float xoffset = xpos - lastX;
-    float yoffset = lastY - ypos; // reversed since y-coordinates go from bottom to top
+    float xOffset = xPos - lastX;
+    float yOffset = lastY - yPos; // reversed since y-coordinates go from bottom to top
 
-    lastX = xpos;
-    lastY = ypos;
+    lastX = xPos;
+    lastY = yPos;
 
-    camera.ProcessMouseMovement(xoffset, yoffset);
+    camera.ProcessMouseMovement(xOffset, yOffset);
 }
 
 // glfw: whenever the mouse scroll wheel scrolls, this callback is called
 // ----------------------------------------------------------------------
-void scroll_callback(GLFWwindow* window, double xoffset, double yoffset)
+void scroll_callback(GLFWwindow* window, double xOffset, double yOffset)
 {
-    camera.ProcessMouseScroll(static_cast<float>(yoffset));
+    camera.ProcessMouseScroll(static_cast<float>(yOffset));
 }
 
 void drawScene(GLFWwindow *window) {
-    float currentFrame = static_cast<float>(glfwGetTime());
+    const auto currentFrame = static_cast<float>(glfwGetTime());
     deltaTime = currentFrame - lastFrame;
     lastFrame = currentFrame;
 
@@ -113,12 +95,10 @@ void drawScene(GLFWwindow *window) {
     glUniformMatrix4fv(sp->u("V"), 1, GL_FALSE, &view[0][0]);
 
     // render the loaded model
-    glm::mat4 model = glm::mat4(1.0f);
+    auto model = glm::mat4(1.0f);
     model = glm::translate(model, glm::vec3(0.0f, 0.0f, 0.0f)); // translate it down so it's at the center of the scene
-    // model = glm::rotate(model, glm::radians(-90.0f), glm::vec3(1.0f, 0.0f, 0.0f));
-    // model = glm::scale(model, glm::vec3(0.01f, 0.01f, 0.01f));	// it's a bit too big for our scene, so scale it down
     glUniformMatrix4fv(sp->u("M"), 1, GL_FALSE, &model[0][0]);
-    backpackModel->Draw(*sp);
+    trackModel->Draw(*sp);
 }
 
 int main() {
@@ -158,7 +138,7 @@ int main() {
     stbi_set_flip_vertically_on_load(true);
     glEnable(GL_DEPTH_TEST);
 
-    backpackModel = new Model("spielberg.glb");
+    trackModel = new Model("spielberg.glb");
     sp = new Shader("v_simplest.glsl", nullptr, "f_simplest.glsl");
 
     // glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
@@ -172,7 +152,7 @@ int main() {
     glfwDestroyWindow(window);
     glfwTerminate();
 
-    delete backpackModel;
+    delete trackModel;
 
     exit(EXIT_SUCCESS);
 }
