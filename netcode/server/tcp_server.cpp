12ae997da7c23b076df07653dcfb021be693fe67
Dawid Grąbczewski <dawid.grabczewski@gmail.com>
2025-12-07 17:30:15 +0000

    created tcp template

---
+++ netcode/server/tcp_server.cpp
@@ -0,0 +1,95 @@
+#include "tcp_server.hpp"
+#include <arpa/inet.h>
+#include <unistd.h>
+#include <cstring>
+#include <iostream>
+#include <utility>
+#include <netdb.h>
+
+TCPServer::TCPServer(std::shared_ptr<ClientManager> clientManager) {
+    socketFd = ::socket(AF_INET, SOCK_STREAM, 0);
+    if (socketFd < 0)
+        throw std::runtime_error(std::string("Failed to create TcpBSDServer socket! ") + std::strerror(errno));
+
+    constexpr int one = 1;
+    setsockopt(socketFd, SOL_SOCKET, SO_REUSEADDR, &one, sizeof(one));
+
+    this->clientManager = std::move(clientManager);
+};
+
+TCPServer::~TCPServer() {
+    if (socketFd >= 0)
+        close(socketFd);
+}
+
+void TCPServer::listen(const char *port) {
+    addrinfo *res, hints{};
+    hints.ai_socktype = SOCK_STREAM;
+    hints.ai_family = AF_INET;
+
+    if (const int rv = getaddrinfo(nullptr, port, &hints, &res))
+        throw std::runtime_error(std::string("TcpBSDServer getaddrinfo failed: ") + gai_strerror(rv));
+
+    if (::bind(socketFd, res->ai_addr, res->ai_addrlen))
+        throw std::runtime_error(std::string("TcpBSDServer bind failed: ") + strerror(errno));
+
+    if (::listen(socketFd, SOMAXCONN))
+        throw std::runtime_error(std::string("TcpBSDServer listen failed: ") + strerror(errno));
+
+    loop();
+}
+
+void TCPServer::addMessageListener(const PacketListener listener) {
+    listeners.push_back(listener);
+}
+
+void TCPServer::send(const ClientHandle client, const char *data, const ssize_t size) const {
+    if (!client.connected) {
+        std::cout << "Tried to send to not connected" << std::endl;
+        return;
+    }
+
+    const ssize_t bytesSent = ::sendto(socketFd, data, size, 0, reinterpret_cast<const sockaddr *>(&client.address),
+                                       sizeof(client.address));
+
+    if (bytesSent <= 0)
+        throw std::runtime_error(std::string("Failed to UDP message: ") + strerror(errno));
+}
+
+[[noreturn]]
+void TCPServer::loop() const {
+    while (true) {
+        auto buf = std::make_unique<char[]>(MAX_PACKET_SIZE);
+
+        sockaddr_in sender = {};
+        socklen_t senderSize = sizeof(sender);
+
+        const ssize_t bytesRead = recvfrom(socketFd, buf.get(), MAX_PACKET_SIZE, 0,
+                                           reinterpret_cast<sockaddr *>(&sender), &senderSize);
+
+        if (bytesRead < 0) {
+            perror("recvfrom");
+            continue;
+        }
+
+        auto client = clientManager->getClient(sender);
+
+        if (!client) {
+            client = clientManager->newClient(sender);
+        }
+
+        auto packet = Packet{
+            .data = std::move(buf),
+            .size = bytesRead,
+            .sender = client
+        };
+
+        parsePacket(packet);
+    }
+}
+
+void TCPServer::parsePacket(const Packet &packet) const {
+    write(STDOUT_FILENO, packet.data.get(), packet.size);
+    send(*packet.sender, "Welcome", 7);
+}
+
---
+++ netcode/server/tcp_server.hpp
@@ -0,0 +1,26 @@
+#pragma once
+
+#include "bsd_server.hpp"
+#include <string>
+
+#define MAX_PACKET_SIZE 1024
+
+class TCPServer final : public BSDServer {
+public:
+    explicit TCPServer(std::shared_ptr<ClientManager> clientManager);
+
+    ~TCPServer() override;
+
+    void listen(const char *port) override;
+
+    void addMessageListener(std::function<void(const Packet &)>) override;
+
+    void send(ClientHandle client, const char *data, ssize_t size) const override;
+
+private:
+    int socketFd;
+
+    [[noreturn]] void loop() const;
+
+    void parsePacket(const Packet &packet) const;
+};
